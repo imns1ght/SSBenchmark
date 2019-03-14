9048c1ba1edde50a6ea193d2bbe3fa9b2aebee1d
imns1ght <jeffersonbrunoit@gmail.com>
2019-03-13 21:02:57 -0300

    Fix average

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(void) {
 	auto sampleRemainder = arraySize % samplesN;	// If remainder exists, save to increment each sample.
 	auto sampleSize = sampleIncrement;		// Size of the samples
 
-	header << "During time (ms)" << setw(20) << "Sample Size";
+	header << "During time (ms)" << "\t" << "Sample Size";
 	// Performs searches while n samples are not reached
 	for (auto i = 0; i < samplesN; i++) {
 		long *result;				// Result of the search
@@ -65,29 +65,29 @@ int main(void) {
 		std::chrono::nanoseconds average {0};	// Average of 100 searches for each sample
 
 		std::cout << "\033[1;35m>>>\033[0m SAMPLE [" << i+1 << "] [" 
-			  << beginRange << ":" << sampleSize << ") \033[1;35m<<<\033[0m";
+			  << beginRange << ":" << sampleSize << ") \033[1;35m<<<\033[0m" << endl;
 
 		for (auto j = 1; j <= 100; j++) {
     			auto start = std::chrono::steady_clock::now();	// Start the clock
 			result = callSearch(first, (array + sampleSize), searchValue, choice);
     			auto end = std::chrono::steady_clock::now();	// Stop the clock
 			sum += end - start;	// Sums the diff between start and end
-			average = sum / j;	// Calculates the progressive average
+			average += sum / j;	// Calculates the progressive average
 		}
 
 		if (result == NULL) { // If the value is not found 	
-			cout << endl << "\033[1;35m>>>\033[0m Value " << searchValue	     
+			cout << "\033[1;35m>>>\033[0m Value " << searchValue	     
 			     << " NOT FOUND at array in" << endl;
 
 		} else { // If the value is not found 		
-			cout << endl << "\033[1;35m>>>\033[0m Value " << searchValue	     
+			cout << "\033[1;35m>>>\033[0m Value " << searchValue	     
 			     << " FOUND at array[" << (*result - beginRange) << "] in" << endl;
 		}	
 
 		// Print the search duration time to terminal
 		printDuration(average); 
 		// Print the search duration time to file
-		numbers << fixed << std::chrono::duration <double, std::milli> (average).count() << setw(28)
+		numbers << fixed << std::chrono::duration <double, std::milli> (average).count() << "\t\t"
 		<< right << sampleSize << endl; 
 		
 		// If remainder exists, increment the sample size.
